fd8f74ef6cd975133d6da00e91fe813ad0e56cf4
Adrian Chen <kynixcn@gmail.com>
2025-12-05 23:24:55 +0800

    Initial Commit

---
+++ ClipboardSync/ClipboardSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardSyncApp.swift
+//  ClipboardSync
+//
+//  Created by Adrian Chen on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
